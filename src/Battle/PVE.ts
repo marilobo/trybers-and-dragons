a3a7dfd2a7778f795de41af44c4d317606a85049
Mariana <mlobosilva@outlook.com>
2023-03-26 12:15:09 -0300

    Feat: cria uma classe dragão

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -10,15 +10,12 @@ export default class PVE extends Battle {
   }
 
   fight(): number {
-    let maxRoundsLoop = 0;
     while (this._player.lifePoints > -1
       && !this._enemies.every((e) => e.lifePoints === -1)) {
       this._enemies.forEach((e) => {
         this._player.attack(e);
         e.attack(this._player);
       });
-      maxRoundsLoop += 1;
-      if (maxRoundsLoop > 100) break;
     }
     return super.fight();
   }
--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  private _lifePoints: number;
+  protected _lifePoints: number;
   private _strength: number;
 
   constructor() {
@@ -13,7 +13,6 @@ export default class Monster implements SimpleFighter {
   get strength() { return this._strength; }
 
   receiveDamage(attackPoints: number): number {
-    // let lifePoints = this._lifePoints;
     if (this._lifePoints <= 0) {
       this._lifePoints = (-1);
     } else {
